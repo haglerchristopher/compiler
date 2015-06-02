05ac3dba6c9314fc86323a2e68fbcf609124f697
Christopher Hagler <haglerchristopher@gmail.com>
2015-06-02 15:22:42 -0400

    added more language symbols

--- main.c
+++ main.c
@@ -8,21 +8,22 @@ FILE *source;
 FILE *listing;
 char token_string[4097];
 
-char buffer[36][15] = {
+char buffer[37][15] = {
 	"ID", "NUM","ELSE","END","ESLE", "FI","FUNCTION","IF","INT",
 	"PRINT","READ","REPEAT","RETURN","SHORT","TEAPER",
 	"THEN","LEFTPAREN","RIGHTPAREN","MULTIPLY","SUBTRACT",
-	"ADD","DIVIDE","MOD","ASSIGN","LESSTHAN","GREATERTHAN","SEMICOLON","RIGHTCRLYBRCE",
+	"ADD","DIVIDE","MOD","ASSIGN","LESSTHAN","GREATERTHAN","QUOTE","SEMICOLON","RIGHTCRLYBRCE",
 	"LEFTCRLYBRCE","QUOTE","AND","OR","XOR","NOT","ENDFILE","ERROR"
 };
 
 int main(int argc, char **argv)
 {
 	TOKENTYPE token;
-	if(argc >= 1) {
+	if(argc >= 2) {
 		source = fopen(argv[1], "r");
 		while((token = get_token()) != ENDFILE) {
 			printf("TokenType: %s, Value: %s\n",buffer[token], token_string);
 		}		
 	}
+	return 0;
 }
